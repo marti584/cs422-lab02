71fa1ec58f0bd59063502c09a08fff8ba84f7156
Hunter Martin <sunnyboy451@gmail.com>
2016-02-03 10:26:20 -0500

    I needed to assign a character to the 4 bytes

--- apps/client.c
+++ apps/client.c
@@ -85,7 +85,7 @@ main(int argc, char *argv[])
 			printf("%d\n", length);
 
 			char paragraph[BUFFSIZE + 4];
-			char size[4] = (char*)&length;
+			char *size = (char*)&length;
 			printf("%s\n", size);
 			// sprintf(paragraph, "%d", length);
 			strcat(paragraph, size);
