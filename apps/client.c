82f90e3a220a106318a3ee2380ade4e1bcbede88
Hunter Martin <sunnyboy451@gmail.com>
2016-02-03 10:02:59 -0500

    Screwed up the strcpy

--- apps/client.c
+++ apps/client.c
@@ -82,7 +82,7 @@ main(int argc, char *argv[])
 			// if (send(conn, length, 4, 0) < 0)
 			// 	printf("Send Failed");
 			char paragraph[BUFFSIZE + 4];
-			sprintf(paragraph, "%d", length));
+			sprintf(paragraph, "%d", length);
 			strcat(paragraph, buff);
 			if ((len2 = send(conn, paragraph, len + 4, 0)) < 0) {
 				printf("Send Failed");
